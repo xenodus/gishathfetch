c8678d7bf15b500b2d6b1f38070cd56ce1b1c216
Alvin Yeoh <alvinyah1@spgroup.com.sg>
2025-03-29 18:40:33 +0800

    Add binderpos gwy test

---
+++ api/gateway/binderpos/binderpos_test.go
@@ -0,0 +1,55 @@
+package binderpos
+
+import (
+	"encoding/json"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_GetCards_Success(t *testing.T) {
+	givenStoreName := "MTG Asia"
+	givenStoreBaseURL := "https://www.mtg-asia.com"
+	givePayload, err := json.Marshal(Payload{
+		StoreURL:    "mtgasia.myshopify.com",
+		Game:        ProductTypeMTG.ToString(),
+		Title:       "Abrade",
+		InstockOnly: true,
+	})
+	require.NoError(t, err)
+
+	cards, httpStatusCode, err := GetCards(givenStoreName, givenStoreBaseURL, givePayload)
+
+	require.NoError(t, err)
+	require.True(t, len(cards) > 0)
+	require.Equal(t, http.StatusOK, httpStatusCode)
+
+	for _, card := range cards {
+		if card.InStock {
+			require.NotEmpty(t, card.Name)
+			require.NotEmpty(t, card.Source)
+			require.NotEmpty(t, card.Url)
+			require.NotEmpty(t, card.Img)
+			require.NotEmpty(t, card.Price)
+		}
+	}
+}
+
+func Test_GetCards_HttpFailure(t *testing.T) {
+	givenStoreName := "ASDF"
+	givenStoreBaseURL := "ASDF"
+	givePayload, err := json.Marshal(Payload{
+		StoreURL:    "ASDF",
+		Game:        ProductTypeMTG.ToString(),
+		Title:       "Abrade",
+		InstockOnly: true,
+	})
+	require.NoError(t, err)
+
+	cards, httpStatusCode, err := GetCards(givenStoreName, givenStoreBaseURL, givePayload)
+
+	require.NoError(t, err)
+	require.Equal(t, 0, len(cards))
+	require.NotEqual(t, http.StatusOK, httpStatusCode)
+}
